584d3a01e59442f0b8ab203483feebb65ab33ad5
Chun-gu <chungu.yi92@gmail.com>
2022-05-16 23:58:13 +0900

    🚚 api 주소 변경

--- src/lib/api/users.ts
+++ src/lib/api/users.ts
@@ -1,14 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
+import { IRegisterUserValues } from "@types";
 
 export const getUsers = async () => {
-  const { data } = await axios.get("http://localhost:1337/api/products");
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/users`
+  );
   return data;
 };
-export const addUser = async () => {
-  const { data } = await axios.get("http://localhost:1337/api/products");
+
+export const addUser = async (values: IRegisterUserValues) => {
+  const { data } = await axios.post(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/auth/local/register`,
+    values
+  );
   return data;
 };
+
 export const deleteUser = async () => {
-  const { data } = await axios.get("http://localhost:1337/api/products");
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/auth/local/register`
+  );
   return data;
 };
