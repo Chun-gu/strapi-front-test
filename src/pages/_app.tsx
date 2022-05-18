f6e4c1b7526d4303523a47343cf775d60c011eb3
Chun-gu <chungu.yi92@gmail.com>
2022-05-18 23:58:02 +0900

    ✨ SessionProvider 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,20 @@ import type { AppProps } from "next/app";
 import { useState } from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { SessionProvider } from "next-auth/react";
 import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <SessionProvider session={session}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SessionProvider>
       </Hydrate>
       <ReactQueryDevtools />
     </QueryClientProvider>
