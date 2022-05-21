de84d0745d2daa88fc0481bbc78cf64f1f7c62df
Chun-gu <chungu.yi92@gmail.com>
2022-05-21 23:58:52 +0900

    👔 next-auth authorize 함수 로직 추가

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,26 +1,53 @@
-import { NextApiRequest } from "next";
+import axios from "axios";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { signIn } from "next-auth/react";
 
 export default NextAuth({
   providers: [
     CredentialsProvider({
+      id: "credentials",
       name: "이메일 혹은 아이디로 로그인",
       credentials: {
         email: {
           label: "Email",
           type: "text",
-          placeholder: "이메일이나 아이디",
         },
         password: {
           label: "Password",
           type: "password",
-          placeholder: "비밀번호",
         },
       },
-      async authorize(credentials: Record<any, any>, req: NextApiRequest) {
-        return credentials;
+      async authorize(
+        credentials: Record<"email" | "password", string> | undefined
+      ) {
+        const { data } = await axios.post(
+          `${process.env.NEXT_PUBLIC_STRAPI_URL}/auth/local`,
+          {
+            identifier: credentials?.email,
+            password: credentials?.password,
+          }
+        );
+        console.log("로그인 결과 정보", data);
+        if (data) return data;
+        else return null;
+        // const { email, password } = credentials as {
+        //   email: string;
+        //   password: string;
+        // };
+        // console.log("아이디, 비밀번호 입력", email, password);
+        // const {
+        //   data: {
+        //     data: { id, nickname, isAdmin },
+        //   },
+        // } = await axios.post(`${process.env.NEXTAUTH_URL}/api/login`, {
+        //   email,
+        //   password,
+        // });
+        // console.log("id, nickname, isAdmin", id, nickname, isAdmin);
+        // if (id) {
+        //   return { id, nickname, isAdmin };
+        // }
+        // throw new Error("로그인 실패");
       },
     }),
   ],
