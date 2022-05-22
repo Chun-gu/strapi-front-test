19b2771edf45a8b83ba412bf3b2a9d40086319e3
Chun-gu <chungu.yi92@gmail.com>
2022-05-22 23:56:36 +0900

    🛂 nextauth와 strapi 연동

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,22 @@
+import { IUser } from "@types";
 import axios from "axios";
-import NextAuth from "next-auth";
+import NextAuth, { Session } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import CredentialsProvider from "next-auth/providers/credentials";
+
+interface ICustomSession extends Session {
+  name?: string | null | undefined;
+  email?: string | null | undefined;
+  image?: string | null | undefined;
+  jwt: string;
+  user: IUser;
+}
+interface ICustomJWT extends JWT {
+  user: {
+    jwt: string;
+    user: IUser;
+  };
+}
 
 export default NextAuth({
   providers: [
@@ -8,8 +24,8 @@ export default NextAuth({
       id: "credentials",
       name: "이메일 혹은 아이디로 로그인",
       credentials: {
-        email: {
-          label: "Email",
+        username: {
+          label: "username",
           type: "text",
         },
         password: {
@@ -18,52 +34,49 @@ export default NextAuth({
         },
       },
       async authorize(
-        credentials: Record<"email" | "password", string> | undefined
+        credentials: Record<"username" | "password", string> | undefined
       ) {
-        const { data } = await axios.post(
-          `${process.env.NEXT_PUBLIC_STRAPI_URL}/auth/local`,
-          {
-            identifier: credentials?.email,
-            password: credentials?.password,
+        try {
+          const { data } = await axios.post(
+            `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/auth/local`,
+            {
+              identifier: credentials?.username,
+              password: credentials?.password,
+            }
+          );
+          console.log("로그인 결과", data);
+          if (data) {
+            return data;
+          } else {
+            return null;
           }
-        );
-        console.log("로그인 결과 정보", data);
-        if (data) return data;
-        else return null;
-        // const { email, password } = credentials as {
-        //   email: string;
-        //   password: string;
-        // };
-        // console.log("아이디, 비밀번호 입력", email, password);
-        // const {
-        //   data: {
-        //     data: { id, nickname, isAdmin },
-        //   },
-        // } = await axios.post(`${process.env.NEXTAUTH_URL}/api/login`, {
-        //   email,
-        //   password,
-        // });
-        // console.log("id, nickname, isAdmin", id, nickname, isAdmin);
-        // if (id) {
-        //   return { id, nickname, isAdmin };
-        // }
-        // throw new Error("로그인 실패");
+        } catch (error) {
+          return null;
+        }
       },
     }),
   ],
   callbacks: {
-    session: async ({ session, token }) => {
-      session.id = token.id;
-      session.jwt = token.jwt;
-      return Promise.resolve(session);
-    },
-    jwt: async ({ token, user }) => {
-      const isSignIn = user ? true : false;
-      if (isSignIn && user) {
-        token.id = user.id;
-        token.jwt = user.jwt;
+    async jwt({ token, user }) {
+      console.log("jwt의 user", user);
+      console.log("jwt의 token", token);
+      if (user) {
+        return {
+          ...token,
+          user,
+        };
       }
-      return Promise.resolve(token);
+      return token;
+    },
+    async session({ session, token }): Promise<Session | ICustomSession> {
+      if (token) {
+        return {
+          ...session,
+          jwt: (token as ICustomJWT).user.jwt,
+          user: (token as ICustomJWT).user.user,
+        };
+      }
+      return session;
     },
   },
   pages: {
