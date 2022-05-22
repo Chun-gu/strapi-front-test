85bc9a84d0e28acb09e27e6090e4353ad9fd61e1
Chun-gu <chungu.yi92@gmail.com>
2022-05-22 23:54:52 +0900

    ✨ session 유무에 따른 분기

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,7 +1,22 @@
+import { getSession, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+
 const NavBar = () => {
+  const { data: session, status } = useSession();
+  if (status === "authenticated") console.log(session);
+
   return (
     <header>
       <button type="button">유저</button>
+      {session ? (
+        <button type="button" onClick={signOut}>
+          로그아웃
+        </button>
+      ) : (
+        <Link href="/login">
+          <button>로그인</button>
+        </Link>
+      )}
     </header>
   );
 };
