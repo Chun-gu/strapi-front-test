cb695f6a7888a9590df094e022634f5e5c71ea1b
Chun-gu <chungu.yi92@gmail.com>
2022-05-23 23:55:13 +0900

    ♻️ addProduct 함수 수정

--- src/lib/api/products.ts
+++ src/lib/api/products.ts
@@ -1,14 +1,33 @@
-import axios from 'axios';
+import axios from "axios";
+import { nanoid } from "nanoid";
+import { IAddProductValues } from "@types";
 
 export const getProducts = async () => {
-  const { data } = await axios.get("http://localhost:1337/api/products");
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/products`
+  );
   return data;
 };
-export const addProduct = async () => {
-  const { data } = await axios.get("http://localhost:1337/api/products");
+
+export const getProduct = async (productId: string) => {
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/products/${productId}`
+  );
   return data;
 };
-export const deleteProduct = async () => {
-  const { data } = await axios.get("http://localhost:1337/api/products");
+
+export const addProduct = async (values: IAddProductValues) => {
+  const productId = `product-${nanoid()}`;
+  const { data } = await axios.post(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/products`,
+    { data: { productId, ...values } }
+  );
+  return data;
+};
+
+export const deleteProduct = async (productId: string) => {
+  const { data } = await axios.delete(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/products/${productId}`
+  );
   return data;
 };
