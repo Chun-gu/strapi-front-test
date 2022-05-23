ddf531684194e71a79c47d044e1d26bf04e24726
Chun-gu <chungu.yi92@gmail.com>
2022-05-23 23:57:17 +0900

    ♻️ product 등록하는 값에 category 추가

--- src/pages/addProduct.tsx
+++ src/pages/addProduct.tsx
@@ -1,35 +1,26 @@
+import { addProduct } from "@api";
 import axios from "axios";
-import { nanoid } from "nanoid";
 import { NextPage } from "next";
 import { useForm } from "react-hook-form";
-
-interface IFormValues {
-  productName: string;
-  option: string;
-  price: number;
-  discountRate: number;
-  stock: number;
-}
+import { IAddProductValues } from "@types";
 
 const AddProduct: NextPage = () => {
-  const { register, handleSubmit } = useForm<IFormValues>();
-  const onSubmit = async (values: IFormValues) => {
-    const { productName, option, price, discountRate, stock } = values;
-    const productId = `productId-${nanoid()}`;
-    const response = await axios.post("http://localhost:1337/api/products", {
-      data: {
-        productId,
-        productName,
-        option,
-        price,
-        discountRate,
-        stock,
-      },
-    });
-    if (response.status === 200) {
-      alert("상품등록 성공");
+  const { register, handleSubmit } = useForm<IAddProductValues>();
+
+  const onSubmit = async (values: IAddProductValues) => {
+    console.log("상품정보", values);
+    try {
+      const data = await addProduct(values);
+      if (data) alert("상품 등록 성공");
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        const errorMessage = (error.response.data as { error: Error }).error
+          .message;
+        alert(`상품등록 실패\n${errorMessage}`);
+      } else {
+        alert("등록 도중에 오류 발생");
+      }
     }
-    console.log(response);
   };
 
   return (
@@ -39,6 +30,14 @@ const AddProduct: NextPage = () => {
         <div>
           <label htmlFor="productName">상품명</label>
           <input type="text" id="productName" {...register("productName")} />
+        </div>
+        <div>
+          <label htmlFor="categories">카테고리</label>
+          <select id="categories" {...register("categories")}>
+            <option value="1">쓴맛</option>
+            <option value="2">신맛</option>
+            <option value="3">단맛</option>
+          </select>
         </div>
         <div>
           <label htmlFor="option">옵션</label>
