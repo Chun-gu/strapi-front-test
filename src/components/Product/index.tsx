afae9ed38a7a60b44232e5e048be73480f19f015
Chun-gu <chungu.yi92@gmail.com>
2022-05-25 23:52:13 +0900

    🔥 불필요한 타입 삭제

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,30 +1,12 @@
-interface IProductProps {
-  id: number;
-  attributes: {
-    productId: string;
-    productName: string;
-    option: string;
-    price: number;
-    discountRate: number;
-    stock: number;
-    description: string;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-  };
-}
+import { IProduct } from "@types";
 
-const Product = (props: IProductProps) => {
+const Product = (props: IProduct) => {
   const { productName, price, discountRate, stock, description } =
     props.attributes;
 
   return (
     <li>
       <h2>{productName}</h2>
-      <span>가격: {price}</span>
-      <span>할인율: {discountRate}</span>
-      <span>재고: {stock}</span>
-      <p>제품 설명: {description}</p>
     </li>
   );
 };
