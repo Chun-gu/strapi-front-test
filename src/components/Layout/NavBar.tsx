dac269380dbcc92cc72773261db56baf211c6d70
Chun-gu <chungu.yi92@gmail.com>
2022-05-28 16:39:03 +0900

    ✨ 페이지 링크 추가

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,12 +1,33 @@
-import { getSession, signOut, useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 const NavBar = () => {
   const { data: session, status } = useSession();
-
+  console.log(session);
   return (
     <header>
-      <button type="button">유저</button>
+      <Link href="/join">
+        <button>회원가입</button>
+      </Link>
+      <Link href="/addProduct">
+        <button>제품등록</button>
+      </Link>
+      <Link href="/products">
+        <button>제품목록</button>
+      </Link>
+      <Link href="/addReview">
+        <button>리뷰등록</button>
+      </Link>
+      <Link href="/addComment">
+        <button>댓글등록</button>
+      </Link>
+      <Link href="/addInquiry">
+        <button>문의등록</button>
+      </Link>
+      <Link href="/addAnswer">
+        <button>답변등록</button>
+      </Link>
+      <button type="button">마이페이지</button>
       {session ? (
         <button type="button" onClick={() => signOut()}>
           로그아웃
