cd25a0e597e22752169710373d32ca35cc83165d
Chun-gu <chungu.yi92@gmail.com>
2022-06-07 23:57:45 +0900

    💄 Navbar 스타일 적용

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,50 +1,64 @@
-import Link from "next/link";
-import { signOut, useSession } from "next-auth/react";
-import * as Styled from "./styled";
+import Link from 'next/link';
+import { signOut, useSession } from 'next-auth/react';
+import * as Styled from './styled';
+import { Tooltip } from '@components';
+import User from 'public/images/icon-user.svg';
+import Logo from 'public/images/logo.svg';
 
 const NavBar = () => {
   const { data: session, status } = useSession();
-  console.log("현재 session 정보", session);
+  console.log('현재 session 정보', session);
   return (
-    <header>
-      <Link href="/join">
-        <button>회원가입</button>
-      </Link>
-      <Link href="/addProduct">
-        <button>제품등록</button>
-      </Link>
-      <Link href="/products">
-        <button>제품목록</button>
-      </Link>
-      <Link href="/addReview">
-        <button>리뷰등록</button>
-      </Link>
-      <Link href="/addComment">
-        <button>댓글등록</button>
-      </Link>
-      <Link href="/addInquiry">
-        <button>문의등록</button>
-      </Link>
-      <Link href="/addAnswer">
-        <button>답변등록</button>
-      </Link>
-      <Link href="/addCart">
-        <button>장바구니 등록</button>
-      </Link>
-      <Link href="/addOrder">
-        <button>주문 등록</button>
-      </Link>
-      <button type="button">마이페이지</button>
-      {session ? (
-        <button type="button" onClick={() => signOut()}>
-          로그아웃
-        </button>
-      ) : (
-        <Link href="/login">
-          <button>로그인</button>
-        </Link>
-      )}
-    </header>
+    <Styled.Header>
+      <Styled.Container>
+        <Logo />
+        <div>
+          <Link href="/join">
+            <button>회원가입</button>
+          </Link>
+          <Link href="/addProduct">
+            <button>제품등록</button>
+          </Link>
+          <Link href="/products">
+            <button>제품목록</button>
+          </Link>
+          <Link href="/addReview">
+            <button>리뷰등록</button>
+          </Link>
+          <Link href="/addComment">
+            <button>댓글등록</button>
+          </Link>
+          <Link href="/addInquiry">
+            <button>문의등록</button>
+          </Link>
+          <Link href="/addAnswer">
+            <button>답변등록</button>
+          </Link>
+          <Link href="/addCart">
+            <button>장바구니 등록</button>
+          </Link>
+          <Link href="/addOrder">
+            <button>주문 등록</button>
+          </Link>
+        </div>
+        <div>
+          <Tooltip>
+            <User />
+            <button type="button">마이페이지</button>
+          </Tooltip>
+
+          {session ? (
+            <button type="button" onClick={() => signOut()}>
+              로그아웃
+            </button>
+          ) : (
+            <Link href="/login">
+              <button>로그인</button>
+            </Link>
+          )}
+        </div>
+      </Styled.Container>
+    </Styled.Header>
   );
 };
 
--- src/components/Layout/Navbar/styled.ts
+++ src/components/Layout/Navbar/styled.ts
@@ -1,1 +1,14 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { COLOR } from '@styles/color';
+
+export const Header = styled.header`
+  padding: 2.5rem 0;
+  background-color: ${COLOR.white};
+`;
+
+export const Container = styled.div`
+  width: 128rem;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+`;
