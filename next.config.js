e801711c96e5431c3ef64a3042800687490b0ad1
Chun-gu <chungu.yi92@gmail.com>
2022-06-08 23:53:27 +0900

    🔧 svgr 옵션 추가

--- next.config.js
+++ next.config.js
@@ -7,8 +7,27 @@ const nextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: {
+                      removeViewBox: false,
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      ],
     });
+
     return config;
   },
 };
