558c25e434ad34b9eceaabc5574bc9213ec44c72
Chun-gu <chungu.yi92@gmail.com>
2022-06-08 23:56:14 +0900

    💄 ImageWrapper에 margin 추가

--- src/utils/ImageWrapper.ts
+++ src/utils/ImageWrapper.ts
@@ -3,12 +3,14 @@ import styled from 'styled-components';
 interface props {
   width?: number;
   height?: number;
+  margin?: string;
   imgStyle?: string;
 }
 
 export default styled.div<props>`
   width: ${({ width }) => width}rem;
   height: ${({ height }) => height}rem;
+  margin: ${({ margin }) => margin};
   position: relative;
   & > * {
     width: 100%;
