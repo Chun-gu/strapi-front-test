63a699c06224bd331e540dae68116b95fcfaf993
Chun-gu <chungu.yi92@gmail.com>
2022-06-10 15:47:16 +0900

    ✨ UserMenu 컴포넌트 작성

--- src/components/UserMenu/index.tsx
+++ src/components/UserMenu/index.tsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
 import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
+import Tooltip from '../Tooltip';
 import UserIcon from '/public/images/icon-user.svg';
-import ToggleDownIcon from '/public/images/icon-triangle-downward.svg';
-import ToggleUpIcon from '/public/images/icon-triangle-upward.svg';
-import ImageWrapper from '@utils/ImageWrapper';
 import * as Styled from './styled';
 
 const UserMenu = () => {
@@ -19,32 +17,34 @@ const UserMenu = () => {
       {session ? (
         <>
           <Styled.UserButton onClick={toggleTooltip}>
-            <ImageWrapper width={3.2}>
-              <UserIcon />
-            </ImageWrapper>
-            <span className="single-ellipsis">{session.user?.username}</span>
-            {isOpen && (
-              <Styled.Tooltip>
+            <Styled.UserIcon width={32} height={32} />
+            <Styled.Username className="ellipsis-single">
+              {session.user?.username}
+            </Styled.Username>
+          </Styled.UserButton>
+          {isOpen && (
+            <Tooltip isOpen={isOpen} setIsOpen={setIsOpen}>
+              <Styled.List>
                 <li>
-                  <Link href="/mypage">마이페이지</Link>
+                  <Link href="/mypage">
+                    <a>마이페이지</a>
+                  </Link>
                 </li>
                 <li>
                   <button type="button" onClick={() => signOut()}>
                     로그아웃
                   </button>
                 </li>
-              </Styled.Tooltip>
-            )}
-          </Styled.UserButton>
+              </Styled.List>
+            </Tooltip>
+          )}
         </>
       ) : (
         <Link href="/login">
-          <>
-            <ImageWrapper>
-              <UserIcon />
-            </ImageWrapper>
-            로그인
-          </>
+          <Styled.LoginLink>
+            <Styled.UserIcon width={32} height={32} />
+            <span>로그인</span>
+          </Styled.LoginLink>
         </Link>
       )}
     </Styled.Container>
--- src/components/UserMenu/styled.ts
+++ src/components/UserMenu/styled.ts
@@ -1,17 +1,56 @@
-import { COLOR } from '@styles/color';
 import styled from 'styled-components';
+import { COLOR } from '@styles/color';
+import User from '/public/images/icon-user.svg';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  position: relative;
+`;
 
 export const UserButton = styled.button`
   display: flex;
   flex-direction: column;
   align-items: center;
-  position: relative;
   width: 5.6rem;
+  &:hover {
+    svg {
+      path {
+        stroke: ${COLOR.accentColor};
+      }
+    }
+  }
 `;
 
-export const Tooltip = styled.ul`
+export const UserIcon = styled(User)`
+  width: 3.2rem;
+  height: 3.2rem;
+  path {
+    stroke: ${COLOR.black};
+  }
+`;
+
+export const Username = styled.span`
+  width: 100%;
+  vertical-align: top;
+`;
+
+export const LoginLink = styled.a`
+  width: 5.6rem;
+  height: 5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  &:hover {
+    svg {
+      path {
+        stroke: ${COLOR.accentColor};
+      }
+    }
+  }
+`;
+
+export const List = styled.ul`
+  text-align: center;
   position: absolute;
   top: 6rem;
   left: 50%;
@@ -24,4 +63,7 @@ export const Tooltip = styled.ul`
   border-radius: 0.5rem;
   box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.2);
   z-index: 1;
+  li:first-child {
+    margin-bottom: 0.3rem;
+  }
 `;
