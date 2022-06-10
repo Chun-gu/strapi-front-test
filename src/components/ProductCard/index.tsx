81592e39232ef3ca68ee73124060f3eff3167a86
Chun-gu <chungu.yi92@gmail.com>
2022-06-10 15:48:20 +0900

    ✨ ProductCard 컴포넌트 작성

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,17 +1,53 @@
 import ImageWrapper from '@utils/ImageWrapper';
 import Image from 'next/image';
-import ProductImage from '/public/images/product-img-small-1.png';
 import * as Styled from './styled';
+import Link from 'next/link';
+import { IProduct } from '@types';
+import { COLOR } from '@styles/color';
+// import { IProduct } from '@shared/types';
 
-const ProductCard = () => {
+const ProductCard = ({ ...product }: IProduct) => {
+  const { images, price, discountRate } = product.attributes;
+  const currentPrice = (price * ((100 - discountRate) / 100)).toLocaleString();
+
   return (
-    <div>
-      <ImageWrapper width={38} height={38} >
-        <Image src={ProductImage} layout="fill" />
-      </ImageWrapper>
-      <Styled.Name>Hack Your Life 개발자 노트북 파우치</Styled.Name>
-      <Styled.Price>{(29000).toLocaleString()}원</Styled.Price>
-    </div>
+    <Link href={{ pathname: 'products/[id]', query: { id: product.id } }}>
+      <a>
+        <ImageWrapper
+          width={38}
+          height={38}
+          borderRadius={1}
+          borderColor={COLOR.greyC4}
+        >
+          <Image
+            src={images.data[0].attributes.formats.small.url}
+            layout="fill"
+            objectFit="cover"
+            priority={true}
+          />
+        </ImageWrapper>
+        <Styled.Name className="ellipsis-single">
+          {product.attributes.productName}
+        </Styled.Name>
+        <Styled.Price>
+          {discountRate > 0 ? (
+            <>
+              <Styled.CurrentPrice>
+                {currentPrice}
+                <span>원</span>
+              </Styled.CurrentPrice>
+              <Styled.OriginalPrice>{price}원</Styled.OriginalPrice>
+              <Styled.DiscountRate>{discountRate}%</Styled.DiscountRate>
+            </>
+          ) : (
+            <Styled.CurrentPrice>
+              {currentPrice}
+              <span>원</span>
+            </Styled.CurrentPrice>
+          )}
+        </Styled.Price>
+      </a>
+    </Link>
   );
 };
 
--- src/components/ProductCard/styled.ts
+++ src/components/ProductCard/styled.ts
@@ -1,14 +1,39 @@
+import { COLOR } from '@styles/color';
 import styled from 'styled-components';
 
 export const Name = styled.span`
   display: block;
   font-size: 1.8rem;
   line-height: 2.2rem;
-  margin: 1.6rem 0 1rem;
+  margin: 1rem 0 0.5rem;
 `;
 
-export const Price = styled.span`
-  font-weight: 700;
+export const Price = styled.div`
+  /* font-weight: 700;
   font-size: 2.4rem;
   line-height: 3.5rem;
+  span {
+    font-weight: 400;
+    font-size: 1.8rem;
+  } */
+`;
+
+export const CurrentPrice = styled.span`
+  font-weight: 700;
+  font-size: 2.4rem;
+  margin-right: 0.5rem;
+  span {
+    font-weight: 400;
+    font-size: 1.8rem;
+  }
+`;
+
+export const OriginalPrice = styled.span`
+  text-decoration-line: line-through;
+  margin-right: 0.5rem;
+`;
+
+export const DiscountRate = styled.span`
+  font-weight: 700;
+  color: ${COLOR.accentColor};
 `;
