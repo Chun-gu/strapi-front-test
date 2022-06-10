5e95e502131010ef364c180159e0e5c91399af7c
Chun-gu <chungu.yi92@gmail.com>
2022-06-10 15:41:53 +0900

    🔧 images domain에 cloudinary 추가

--- next.config.js
+++ next.config.js
@@ -7,28 +7,13 @@ const nextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            svgoConfig: {
-              plugins: [
-                {
-                  name: 'preset-default',
-                  params: {
-                    overrides: {
-                      removeViewBox: false,
-                    },
-                  },
-                },
-              ],
-            },
-          },
-        },
-      ],
+      use: ['@svgr/webpack'],
     });
 
     return config;
+  },
+  images: {
+    domains: ['res.cloudinary.com'],
   },
 };
 
