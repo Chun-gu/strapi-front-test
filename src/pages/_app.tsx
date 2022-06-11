4d5a880bf8a95d8bc0cdbae005b7741066cac71b
Chun-gu <chungu.yi92@gmail.com>
2022-06-12 00:00:09 +0900

    ✨ queryClient 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,23 @@
-import type { AppProps } from "next/app";
-import { SessionProvider } from "next-auth/react";
-import Layout from "../components/Layout";
-import GlobalStyle from "@styles/GlobalStyle";
+import { useState } from 'react';
+import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import Layout from '../components/Layout';
+import GlobalStyle from '@styles/GlobalStyle';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
-    <SessionProvider session={session}>
-      <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <SessionProvider session={session}>
+          <GlobalStyle />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SessionProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
