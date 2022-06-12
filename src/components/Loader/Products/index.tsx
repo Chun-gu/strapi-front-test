088a8ac9b8536afef0d290a7bceb1852543fe703
Chun-gu <chungu.yi92@gmail.com>
2022-06-12 23:57:13 +0900

    ✨ 메인 페이지의 제품 목록에 대응하는 스켈레톤 로더 구현

---
+++ src/components/Loader/Products/index.tsx
@@ -0,0 +1,55 @@
+import * as Styled from './styled';
+
+const ProductsLoader = () => {
+  return (
+    <Styled.ProductList>
+      <li>
+        <Styled.Image />
+        <Styled.Name />
+        <Styled.Price />
+      </li>
+      <li>
+        <Styled.Image />
+        <Styled.Name />
+        <Styled.Price />
+      </li>
+      <li>
+        <Styled.Image />
+        <Styled.Name />
+        <Styled.Price />
+      </li>
+      <li>
+        <Styled.Image />
+        <Styled.Name />
+        <Styled.Price />
+      </li>
+      <li>
+        <Styled.Image />
+        <Styled.Name />
+        <Styled.Price />
+      </li>
+      <li>
+        <Styled.Image />
+        <Styled.Name />
+        <Styled.Price />
+      </li>
+      <li>
+        <Styled.Image />
+        <Styled.Name />
+        <Styled.Price />
+      </li>
+      <li>
+        <Styled.Image />
+        <Styled.Name />
+        <Styled.Price />
+      </li>
+      <li>
+        <Styled.Image />
+        <Styled.Name />
+        <Styled.Price />
+      </li>
+    </Styled.ProductList>
+  );
+};
+
+export default ProductsLoader;
