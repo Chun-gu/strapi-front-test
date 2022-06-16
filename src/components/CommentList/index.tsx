3b0591146934821d844b5b1a4dde01b57fa049c0
Chun-gu <chungu.yi92@gmail.com>
2022-06-16 23:56:30 +0900

    ✨ 댓글 작성 기능 구현

--- src/components/CommentList/index.tsx
+++ src/components/CommentList/index.tsx
@@ -1,125 +1,122 @@
-import { useSession } from 'next-auth/react';
-import Image from 'next/image';
-import { useState, useEffect, useCallback } from 'react';
-import { useForm } from 'react-hook-form';
-import { nanoid } from 'nanoid';
-import ImageWrapper from '@utils/ImageWrapper';
-import { IComment } from '@shared/types';
-import { CommentItem } from '../CommentItem';
-import * as Buttons from '../Buttons';
-import { Pagination } from '../Pagination';
-import * as Styled from './styled';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { ICommentResponse } from "@types";
+import { addComment, getComments } from "@api";
+import ImageWrapper from "@utils/ImageWrapper";
+import CommentItem from "../CommentItem";
+import * as Buttons from "../Buttons";
+import { Pagination } from "../Pagination";
+import * as Styled from "./styled";
 
 interface ICommentListProps {
-  reviewId: string;
+  reviewId: number;
 }
 
 export function CommentList({ reviewId }: ICommentListProps) {
   const { data: session } = useSession();
+  const { isLoading, data: commentsData } = useQuery<ICommentResponse>(
+    ["getComments", reviewId],
+    () => getComments(reviewId),
+  );
+
   const {
     reset,
     register,
     getValues,
     handleSubmit,
     formState: { isValid },
-  } = useForm<{ comment: string }>({ mode: 'onChange' });
+  } = useForm<{ content: string }>({ mode: "onChange" });
 
   const itemsPerPage = 5;
   const [pageNum, setPageNum] = useState(1);
   const offset = (pageNum - 1) * itemsPerPage;
-  const [isLoading, setIsLoading] = useState(true);
-  const [commentsData, setCommentsData] = useState<IComment[]>();
 
-  const getData = useCallback(async () => {
-    const { data } = await api.get(`/review/comment/${reviewId}`);
-    console.log('comment', data);
-    setCommentsData(data);
-    setIsLoading(false);
-  }, [reviewId]);
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation(addComment, {
+    onSuccess: () => {
+      alert("댓글 작성 성공!");
+      queryClient.invalidateQueries(["getComments", reviewId]);
+      reset();
+    },
+    onError: (error) => {
+      alert(error);
+    },
+  });
 
   const onSubmit = async () => {
-    const { comment: content } = getValues();
-    const { success, message } = await api.post('/comment', {
-      review: reviewId,
-      user: userId,
-      content,
-    });
-    if (success) {
-      reset();
-      getData();
-    } else {
-      console.error(message);
+    const { content } = getValues();
+
+    if (session) {
+      const author = session.user?.id as number;
+      const jwt = session.jwt as string;
+      mutate({ review: reviewId, author, jwt, content });
     }
   };
 
-  useEffect(() => {
-    getData();
-  }, [getData]);
-
+  if (isLoading) return <div>Loading...</div>;
   return (
     <Styled.CommentSection>
-      {isLoading ? (
-        'Loading'
-      ) : (
+      {commentsData && commentsData.comments.length > 0 && (
         <>
-          {commentsData && commentsData!.length > 0 && (
-            <>
-              <ul>
-                {commentsData
-                  .slice(offset, offset + itemsPerPage)
-                  .map((comment) => (
-                    <CommentItem key={nanoid()} {...comment} />
-                  ))}
-              </ul>
-              {commentsData.length > itemsPerPage && (
-                <Styled.PaginationWrapper>
-                  <Pagination
-                    totalItemCount={commentsData.length}
-                    itemsPerPage={itemsPerPage}
-                    pageNum={pageNum}
-                    setPageNum={setPageNum}
-                  />
-                </Styled.PaginationWrapper>
-              )}
-            </>
-          )}
-          <Styled.CommentInputForm onSubmit={handleSubmit(onSubmit)}>
-            <Styled.Author>
-              <ImageWrapper
-                width="4rem"
-                height="4rem"
-                imgStyle="border-radius: 50%;"
-              >
-                <Image src="/images/seller-profileIMG.png" layout="fill" />
-              </ImageWrapper>
-              <span className="ellipsis-single">{nickname}</span>
-            </Styled.Author>
-            <Styled.CommentInput htmlFor="comment">
-              <span className="sr-only">댓글 입력창</span>
-              <input
-                type="text"
-                id="comment"
-                maxLength={100}
-                placeholder="댓글을 입력하세요(10자 이상, 공백 불가)"
-                {...register('comment', {
-                  required: true,
-                  minLength: 10,
-                  pattern: /[^\s]/,
-                })}
+          <ul>
+            {commentsData.comments
+              .slice(offset, offset + itemsPerPage)
+              .map((comment) => (
+                <CommentItem key={comment.id} {...comment} />
+              ))}
+          </ul>
+          {commentsData.comments.length > itemsPerPage && (
+            <Styled.PaginationWrapper>
+              <Pagination
+                totalItemCount={commentsData.comments.length}
+                itemsPerPage={itemsPerPage}
+                pageNum={pageNum}
+                setPageNum={setPageNum}
               />
-            </Styled.CommentInput>
-            <Buttons.Custom
-              width={7}
-              height={3}
-              fontSize={1.4}
-              color="green"
-              disabled={!isValid}
-            >
-              작성
-            </Buttons.Custom>
-          </Styled.CommentInputForm>
+            </Styled.PaginationWrapper>
+          )}
         </>
       )}
+      <Styled.CommentInputForm onSubmit={handleSubmit(onSubmit)}>
+        <Styled.Author>
+          <ImageWrapper width={4} height={4} borderRadius="50%">
+            <Image
+              src="/images/seller-profileIMG.png"
+              layout="fill"
+              alt="작성자 사진"
+            />
+          </ImageWrapper>
+          <span className="ellipsis-single">
+            {session ? session.user?.username : "로그인 필요"}
+          </span>
+        </Styled.Author>
+        <Styled.CommentInput htmlFor="content">
+          <span className="sr-only">댓글 입력창</span>
+          <input
+            type="text"
+            id="content"
+            maxLength={100}
+            placeholder="댓글을 입력하세요(10자 이상, 공백 불가)"
+            {...register("content", {
+              required: true,
+              minLength: 10,
+              pattern: /[^\s]/,
+            })}
+          />
+        </Styled.CommentInput>
+        <Buttons.Custom
+          width={7}
+          height={3}
+          fontSize={1.4}
+          color="green"
+          disabled={!isValid}
+        >
+          작성
+        </Buttons.Custom>
+      </Styled.CommentInputForm>
     </Styled.CommentSection>
   );
 }
