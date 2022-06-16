9833e8a5ca379aed70f29f1df319bc9aa4ba5681
Chun-gu <chungu.yi92@gmail.com>
2022-06-16 23:57:12 +0900

    ✨ 문의에 대한 답변 작성 기능 구현

--- src/components/InquiryItem/index.tsx
+++ src/components/InquiryItem/index.tsx
@@ -1,56 +1,87 @@
-import { useEffect, useState } from 'react';
-import { IInquiry, IUser } from '@shared/types';
-import { dateConverter } from '@utils/dateConverter';
-import * as Buttons from '../Buttons';
-import * as Styled from './styled';
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { IInquiry } from "@types";
+import { dateConverter } from "@utils/dateConverter";
+import { Answer } from "@components";
+import * as Buttons from "../Buttons";
+import * as Styled from "./styled";
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "react-query";
+import { addAnswer } from "@api";
+import { useRouter } from "next/router";
 
-export function InquiryItem({
-  content,
-  answer,
-  createdAt,
-  updatedAt
-}: IInquiry) {
-  const isSeller = false;
-  const isAnswered = !!answer;
+export function InquiryItem({ ...inquiry }: IInquiry) {
+  const { data: session } = useSession();
+  const isSeller = session?.user?.isSeller;
+
+  const router = useRouter();
+  const productId = router.query.id;
+
+  const { id, content, author, answer, createdAt } = inquiry;
+
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation(addAnswer, {
+    onSuccess: () => {
+      alert("답변 작성 성공");
+      queryClient.invalidateQueries(["getInquiries", productId]);
+      reset();
+    },
+    onError: (error) => {
+      alert(error);
+    },
+  });
+
+  const { reset, register, getValues, handleSubmit } = useForm<{
+    content: string;
+  }>({ mode: "onChange" });
+
+  const onSubmit = () => {
+    const { content } = getValues();
+
+    if (session) {
+      const jwt = session.jwt as string;
+      mutate({ jwt, content, inquiry: id });
+    }
+  };
+
   const [isOpen, setIsOpen] = useState(false);
-  const [userData, setUserData] = useState<IUser>();
   const toggleOpen = () => {
     setIsOpen(() => !isOpen);
   };
-  const getData = async () => {
-    const { data } = await api.get(`/user/${userId}`);
-    setUserData(data);
-  };
-  useEffect(() => {
-    getData();
-  }, []);
 
   return (
     <Styled.Inquiry>
-      <span>{isAnswered ? '답변완료' : '답변대기'}</span>
+      <span>{answer ? "답변완료" : "답변대기"}</span>
       <Styled.InquiryContent
         onClick={toggleOpen}
         isOpen={isOpen}
-        isAnswered={isAnswered}
+        isAnswered={!!answer}
       >
         {content}
       </Styled.InquiryContent>
-      <span className="ellipsis-single">{userData?.userName}</span>
+      <span className="ellipsis-single">{author.username}</span>
       <span>{dateConverter(createdAt)}</span>
-      {isOpen && isAnswered && (
+      {/* {isOpen && <Answer answerId={answer?.id} />} */}
+      {isOpen && !!answer && (
         <Styled.Answer>
           <Styled.AnswerContent>
-            <Styled.Badge>답변</Styled.Badge>
-            {answer}
+            <>
+              <Styled.Badge>답변</Styled.Badge>
+              <span>{answer.content}</span>
+            </>
           </Styled.AnswerContent>
           <span>판매자</span>
-          <span>{dateConverter(updatedAt)}</span>
+          <span>{dateConverter(createdAt)}</span>
         </Styled.Answer>
       )}
-      {isOpen && isSeller && !isAnswered && (
+      {isOpen && !!isSeller && !answer && (
         <Styled.Answer>
-          <Styled.AnswerForm>
-            <Styled.AnswerInput type="text" placeholder="답변 작성" />
+          <Styled.AnswerForm onSubmit={handleSubmit(onSubmit)}>
+            <Styled.AnswerInput
+              type="text"
+              placeholder="답변 작성"
+              {...register("content")}
+            />
             <Buttons.Custom
               width={7}
               height={3}
