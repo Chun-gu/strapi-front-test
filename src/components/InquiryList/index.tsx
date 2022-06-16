033d4ec0eb11f5835d6a2080c376ced41beff45c
Chun-gu <chungu.yi92@gmail.com>
2022-06-16 23:57:51 +0900

    ✨ 문의 목록 가져오기 react-query로 변경

--- src/components/InquiryList/index.tsx
+++ src/components/InquiryList/index.tsx
@@ -1,41 +1,71 @@
-import { useState } from 'react';
-import { nanoid } from 'nanoid';
-import { IInquiry } from '@shared/types';
-import { InquiryItem } from '../InquiryItem';
-// import questionData from './dummyQuestionData.json';
-import { Pagination } from '../Pagination';
-import * as Styled from './styled';
+import { useState } from "react";
+import { dehydrate, QueryClient, useQuery } from "react-query";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import { getInquiries } from "@api";
+import { InquiryItem, Pagination } from "@components";
+import { IInquiriesResponse } from "@types";
+import { NoneYet } from "@styles/GlobalStyle";
+import * as Styled from "./styled";
 
-interface IInquiryListProps {
-  inquiries: IInquiry[];
-}
+export function InquiryList() {
+  const router = useRouter();
+  const { productId } = router.query;
 
-export function InquiryList({ inquiries }: IInquiryListProps) {
+  const { data } = useQuery<IInquiriesResponse>(
+    ["getInquiries", productId],
+    () => getInquiries(productId),
+  );
+
   const itemsPerPage = 5;
   const [pageNum, setPageNum] = useState(1);
   const offset = (pageNum - 1) * itemsPerPage;
 
   return (
     <>
-      <Styled.List>
-        <li>
-          <span aria-hidden="true">답변상태</span>
-          <span aria-hidden="true">질문</span>
-          <span aria-hidden="true">작성자</span>
-          <span aria-hidden="true">작성일</span>
-        </li>
-        {inquiries.slice(offset, offset + itemsPerPage).map((inquiry) => (
-          <InquiryItem key={nanoid()} {...inquiry} />
-        ))}
-      </Styled.List>
-      {inquiries.length > itemsPerPage && (
-        <Pagination
-          totalItemCount={inquiries.length}
-          itemsPerPage={itemsPerPage}
-          pageNum={pageNum}
-          setPageNum={setPageNum}
-        />
+      {data && data?.inquiries.length !== 0 ? (
+        <>
+          <Styled.List>
+            <li>
+              <span aria-hidden="true">답변상태</span>
+              <span aria-hidden="true">질문</span>
+              <span aria-hidden="true">작성자</span>
+              <span aria-hidden="true">작성일</span>
+            </li>
+            {data.inquiries
+              .slice(offset, offset + itemsPerPage)
+              .map((inquiry) => (
+                <InquiryItem key={`inquiry-${inquiry.id}`} {...inquiry} />
+              ))}
+          </Styled.List>
+          {data.inquiries.length > itemsPerPage && (
+            <Pagination
+              totalItemCount={data.inquiries.length}
+              itemsPerPage={itemsPerPage}
+              pageNum={pageNum}
+              setPageNum={setPageNum}
+            />
+          )}
+        </>
+      ) : (
+        <NoneYet>아직 문의가 없습니다.</NoneYet>
       )}
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const productId = ctx.params?.productId;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(["getInquiries", productId], () =>
+    getInquiries(productId),
+  );
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
+
+export default InquiryList;
