317d2770e13ed39df8ea4b861d8bbdb6a0a99fd6
Chun-gu <chungu.yi92@gmail.com>
2022-06-17 23:45:43 +0900

    🩹 key 값 변경

--- src/components/TabMenu/index.tsx
+++ src/components/TabMenu/index.tsx
@@ -1,7 +1,6 @@
-import { useState, useEffect, useRef } from 'react';
-import { nanoid } from 'nanoid';
-import * as Buttons from '../Buttons';
-import * as Styled from './styled';
+import { useState, useEffect, useRef } from "react";
+import * as Buttons from "../Buttons";
+import * as Styled from "./styled";
 
 interface ITabMenuProps {
   sectionRefs: {
@@ -10,7 +9,7 @@ interface ITabMenuProps {
 }
 
 export function TabMenu({ sectionRefs: { current } }: ITabMenuProps) {
-  const TabMenuDatas = ['제품 상세 정보', '리뷰', 'Q&A', '교환/반품 정보'];
+  const TabMenuDatas = ["제품 상세 정보", "리뷰", "Q&A", "교환/반품 정보"];
   const [activeTab, setActiveTab] = useState<HTMLElement | Element>();
   const tabMenu = useRef<HTMLDivElement>(null);
 
@@ -28,7 +27,7 @@ export function TabMenu({ sectionRefs: { current } }: ITabMenuProps) {
     const observerOption = { threshold: 0.6 };
     const observer = new IntersectionObserver(
       changeSelectedTab,
-      observerOption
+      observerOption,
     );
 
     current.forEach((tab) => observer.observe(tab));
@@ -37,13 +36,13 @@ export function TabMenu({ sectionRefs: { current } }: ITabMenuProps) {
   }, [current]);
 
   const handleClick = (index: number) => {
-    const navHeight = document.getElementsByTagName('nav')[0].offsetHeight;
-    const tabMenuHeight = tabMenu!.current!.offsetHeight;
+    const navHeight = document.getElementsByTagName("nav")[0].offsetHeight;
+    const tabMenuHeight = tabMenu.current?.offsetHeight as number;
 
     window.scrollTo({
       top: current[index].offsetTop - navHeight - tabMenuHeight,
       left: 0,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
     setActiveTab(current[index]);
   };
@@ -52,7 +51,7 @@ export function TabMenu({ sectionRefs: { current } }: ITabMenuProps) {
     <Styled.TabMenu ref={tabMenu}>
       {TabMenuDatas.map((data, index) => (
         <Buttons.Tab
-          key={nanoid()}
+          key={`tab-${index}`}
           isActive={activeTab === current[index]}
           onClick={() => handleClick(index)}
         >
