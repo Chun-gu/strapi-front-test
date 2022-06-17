5c5ff7890cf3e01ae69e5c9cbbd22cf47c296c19
Chun-gu <chungu.yi92@gmail.com>
2022-06-17 23:44:56 +0900

    ✨ 리뷰 목록 구현

--- src/components/ReviewList/index.tsx
+++ src/components/ReviewList/index.tsx
@@ -1,36 +1,63 @@
-import { useState } from 'react';
-import { nanoid } from 'nanoid';
-import { IReview } from '@shared/types';
-import { ReviewItem } from '../ReviewItem';
-import { Pagination } from '../Pagination';
-import * as Styled from './styled';
+import { useState } from "react";
+import { dehydrate, QueryClient, useQuery } from "react-query";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import { getReviews } from "@api";
+import { ReviewItem, Pagination } from "@components";
+import { IReviewResponse } from "@types";
+import * as Styled from "./styled";
 
-interface IReviewListProps {
-  reviews: IReview[];
-}
+export function ReviewList() {
+  const router = useRouter();
+  const { productId } = router.query;
+  const { data: reviewsData } = useQuery<IReviewResponse>(
+    ["getReviews", productId],
+    () => getReviews(productId),
+  );
 
-export function ReviewList({ reviews }: IReviewListProps) {
   const itemsPerPage = 5;
   const [pageNum, setPageNum] = useState(1);
   const offset = (pageNum - 1) * itemsPerPage;
 
   return (
     <>
-      <Styled.ReviewList>
-        <ul>
-          {reviews.slice(offset, offset + itemsPerPage).map((review) => (
-            <ReviewItem key={nanoid()} {...review} />
-          ))}
-        </ul>
-      </Styled.ReviewList>
-      {reviews.length > itemsPerPage && (
-        <Pagination
-          totalItemCount={reviews.length}
-          itemsPerPage={itemsPerPage}
-          pageNum={pageNum}
-          setPageNum={setPageNum}
-        />
+      {reviewsData && reviewsData?.reviews.length !== 0 ? (
+        <>
+          <Styled.ReviewList>
+            <ul>
+              {reviewsData.reviews
+                .slice(offset, offset + itemsPerPage)
+                .map((review) => (
+                  <ReviewItem key={review.id} {...review} />
+                ))}
+            </ul>
+          </Styled.ReviewList>
+          {reviewsData && reviewsData.reviews.length > itemsPerPage && (
+            <Pagination
+              totalItemCount={reviewsData.reviews.length}
+              itemsPerPage={itemsPerPage}
+              pageNum={pageNum}
+              setPageNum={setPageNum}
+            />
+          )}
+        </>
+      ) : (
+        <Styled.NoneYet>아직 리뷰가 없습니다.</Styled.NoneYet>
       )}
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const productId = ctx.params?.productId;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(["getReviews", productId], () =>
+    getReviews(productId),
+  );
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
