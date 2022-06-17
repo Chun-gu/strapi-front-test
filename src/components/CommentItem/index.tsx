7325209e957ae7f726d9eec998494b5145b06c50
Chun-gu <chungu.yi92@gmail.com>
2022-06-17 23:38:52 +0900

    🩹 ImageWrapper 속성값 변경 및 변수명 변경

--- src/components/CommentItem/index.tsx
+++ src/components/CommentItem/index.tsx
@@ -1,22 +1,29 @@
-import Image from 'next/image';
-import ImageWrapper from '@utils/ImageWrapper';
-import { IComment } from '@shared/types';
-import { dateConverter } from '@utils/dateConverter';
-import authorImg from 'public/images/product-img-small-1.png';
-import * as Styled from './styled';
+import Image from "next/image";
+import ImageWrapper from "@utils/ImageWrapper";
+import { IComment } from "@types";
+import { dateConverter } from "@utils/dateConverter";
+import authorImg from "public/images/product-img-small-1.png";
+import * as Styled from "./styled";
 
-export function CommentItem({
-  user: { nickname },
-  content,
-  createdAt,
-}: IComment) {
+export default function CommentItem({ ...comment }: IComment) {
+  const {
+    author: { username },
+    content,
+    createdAt,
+  } = comment;
+
   return (
     <Styled.CommentItem>
       <Styled.Author>
-        <ImageWrapper width="4rem" height="4rem" imgStyle="border-radius: 50%;">
-          <Image src={authorImg} layout="fill" />
+        <ImageWrapper width={4} height={4} borderRadius="50%">
+          <Image
+            src={authorImg}
+            layout="fill"
+            objectFit="cover"
+            alt="작성자 사진"
+          />
         </ImageWrapper>
-        <span>{nickname}</span>
+        <span>{username}</span>
       </Styled.Author>
       <Styled.Content>{content}</Styled.Content>
       <Styled.Date>{dateConverter(createdAt)}</Styled.Date>
