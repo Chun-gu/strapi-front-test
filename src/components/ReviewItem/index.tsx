4e8293682dcf8605d31a42c1fceab345dff094c9
Chun-gu <chungu.yi92@gmail.com>
2022-06-17 23:44:17 +0900

    🩹 ImageWrapper, Image 속성값 변경

--- src/components/ReviewItem/index.tsx
+++ src/components/ReviewItem/index.tsx
@@ -1,38 +1,32 @@
-import { useState } from 'react';
-import Image from 'next/image';
-import { dateConverter } from '@utils/dateConverter';
-import { IReview } from '@shared/types';
-import ImageWrapper from '@utils/ImageWrapper';
-import authorImg from 'public/images/product-img-small-1.png';
-import reviewImg from 'public/images/product-img-lg.png';
-import StarRating from '../StarRating';
-import { CommentList } from '../CommentList';
-import * as Styled from './styled';
+import { useState } from "react";
+import Image from "next/image";
+import { IReview } from "@types";
+import ImageWrapper from "@utils/ImageWrapper";
+import { dateConverter } from "@utils/dateConverter";
+import StarRating from "../StarRating";
+import { CommentList } from "../CommentList";
+import authorImg from "public/images/product-img-small-1.png";
+import * as Styled from "./styled";
 
-export function ReviewItem({
-  _id,
-  user: { nickname },
-  rating,
-  images,
-  content,
-  createdAt
-}: IReview) {
+export function ReviewItem({ ...review }: IReview) {
   const [isOpen, setIsOpen] = useState(false);
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
+  const { id, author, rating, content, images, createdAt } = review;
+
   return (
     <Styled.ReviewContainer>
       <Styled.ReviewInfo>
-        <ImageWrapper width="4rem" height="4rem" imgStyle="border-radius: 50%;">
-          <Image src={authorImg} layout="fill" />
+        <ImageWrapper width={4} height={4} borderRadius="50%">
+          <Image src={authorImg} layout="fill" alt="작성자 사진" />
         </ImageWrapper>
         <div>
           <Styled.RatingWrapper>
             <StarRating rating={rating} readOnly />
           </Styled.RatingWrapper>
-          <Styled.Author>{nickname}</Styled.Author>
+          <Styled.Author>{author.username}</Styled.Author>
           <Styled.Date>{dateConverter(createdAt)}</Styled.Date>
         </div>
       </Styled.ReviewInfo>
@@ -41,20 +35,30 @@ export function ReviewItem({
         {isOpen && (
           <>
             {images.length > 0 && (
-              <ImageWrapper width="40rem" height="40rem">
-                <Image src={reviewImg} layout="fill" />
+              <ImageWrapper width={40} height={40}>
+                <Image
+                  src={images[0].medium}
+                  layout="fill"
+                  objectFit="cover"
+                  alt="리뷰 사진"
+                />
               </ImageWrapper>
             )}
-            <CommentList reviewId={_id} />
+            <CommentList reviewId={id} />
           </>
         )}
         <Styled.ExpansionButton onClick={handleClick} isOpen={isOpen}>
-          {isOpen ? '접 기' : '더보기'}
+          {isOpen ? "접 기" : "더보기"}
         </Styled.ExpansionButton>
       </Styled.ReviewContent>
       {!isOpen && (
-        <ImageWrapper width="8rem" height="8rem">
-          <Image src={reviewImg} layout="fill" />
+        <ImageWrapper width={8} height={8}>
+          <Image
+            src={images[0].thumbnail}
+            layout="fill"
+            objectFit="cover"
+            alt="리뷰 사진"
+          />
         </ImageWrapper>
       )}
     </Styled.ReviewContainer>
