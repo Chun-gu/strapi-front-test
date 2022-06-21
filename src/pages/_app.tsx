c6df6d5191bff416016edd9e291593d8be42bd10
Chun-gu <chungu.yi92@gmail.com>
2022-06-21 23:52:28 +0900

    ✨ recoilRoot 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
-import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
-import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import Layout from '../components/Layout';
-import GlobalStyle from '@styles/GlobalStyle';
+import { useState } from "react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
+import { Layout } from "@components";
+import GlobalStyle from "@styles/GlobalStyle";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [queryClient] = useState(
@@ -21,12 +22,14 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <SessionProvider session={session}>
-          <GlobalStyle />
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </SessionProvider>
+        <RecoilRoot>
+          <SessionProvider session={session}>
+            <GlobalStyle />
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </SessionProvider>
+        </RecoilRoot>
       </Hydrate>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
