200c2a42c5d2cf771f0a621cd75341098c4038ca
Chun-gu <chungu.yi92@gmail.com>
2022-06-22 23:56:07 +0900

    ✨ useIntersectionObserver 훅 작성

---
+++ src/utils/useIntersectionObserver.ts
@@ -0,0 +1,29 @@
+import { RefObject, useEffect, useState } from "react";
+
+function useIntersectionObserver(
+  targetRef: RefObject<Element>,
+  { root = null, rootMargin = "0%", threshold = 0 }: IntersectionObserverInit,
+): IntersectionObserverEntry | undefined {
+  const [entry, setEntry] = useState<IntersectionObserverEntry>();
+  console.log(entry);
+  const onIntersect = ([entry]: IntersectionObserverEntry[]): void => {
+    setEntry(entry);
+  };
+
+  useEffect(() => {
+    const target = targetRef?.current;
+
+    if (!target) return;
+
+    const options = { root, rootMargin, threshold };
+    const observer = new IntersectionObserver(onIntersect, options);
+
+    observer.observe(target);
+
+    return () => observer.disconnect();
+  }, [root, rootMargin, targetRef, threshold]);
+
+  return entry;
+}
+
+export default useIntersectionObserver;
