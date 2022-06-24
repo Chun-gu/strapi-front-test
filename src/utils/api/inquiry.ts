562d4a67db0cf2a0407002522c08b8be0c3ef641
Chun-gu <chungu.yi92@gmail.com>
2022-06-24 23:55:38 +0900

    👽️ getInquiries에 limit, page 쿼리 추가

--- src/utils/api/inquiry.ts
+++ src/utils/api/inquiry.ts
@@ -1,30 +1,30 @@
-import { IAddInquiryValues } from "src/types";
 import axios from "axios";
+import { IAddInquiryValues } from "@types";
+
+export const getInquiries = async (
+  productId: number | string | string[] | undefined,
+  limit = 5,
+  page = 1,
+) => {
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/product-inquiries/${productId}?limit=${limit}&page=${page}`,
+  );
+
+  return data;
+};
 
 export const addInquiry = async (
   jwt: string,
   author: number,
-  values: IAddInquiryValues
+  values: IAddInquiryValues,
 ) => {
   const { data } = await axios.post(
     `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/inquiries`,
     { data: { author, ...values } },
-    { headers: { Authorization: `Bearer ${jwt}` } }
+    { headers: { Authorization: `Bearer ${jwt}` } },
   );
 
   return data;
 };
 
-export const addAnswer = async (
-  jwt: string,
-  inquiryId: string,
-  values: string
-) => {
-  const { data } = await axios.put(
-    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/inquiries/${inquiryId}`,
-    { data: { answer: values } },
-    { headers: { Authorization: `Bearer ${jwt}` } }
-  );
-
-  return data;
-};
+// export const updateInquiry = async () => {return data};
