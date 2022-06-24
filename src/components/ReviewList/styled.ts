4c34b0fe1de2d3248803837048746e68d8e014a7
Chun-gu <chungu.yi92@gmail.com>
2022-06-24 23:58:01 +0900

    💄 ReviewList 스타일 추가

--- src/components/ReviewList/styled.ts
+++ src/components/ReviewList/styled.ts
@@ -1,5 +1,21 @@
 import styled from "styled-components";
+import { COLOR } from "@styles/color";
 
-export const ReviewList = styled.div`
+export const ReviewSection = styled.div`
+  position: relative;
+  min-height: 66.4rem;
+`;
+
+export const ReviewList = styled.ul`
   margin-bottom: 4rem;
+`;
+
+export const NoneYetWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 128rem;
+  height: 60.6rem;
+  border-top: 1px solid ${COLOR.greyC4};
+  border-bottom: 1px solid ${COLOR.greyC4};
 `;
