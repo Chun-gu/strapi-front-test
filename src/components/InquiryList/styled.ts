fe21e4da409a1ec2fbbcd01fdea4621d98cad74b
Chun-gu <chungu.yi92@gmail.com>
2022-06-24 23:54:09 +0900

    💄 문의목록 레이아웃 변경

--- src/components/InquiryList/styled.ts
+++ src/components/InquiryList/styled.ts
@@ -1,10 +1,24 @@
 import { COLOR } from "@shared/constants";
 import styled from "styled-components";
 
-export const List = styled.ul`
-  min-height: 31.8rem;
+export const InquirySection = styled.div`
   font-size: 1.6rem;
   line-height: 1.3;
+  text-align: center;
+  position: relative;
+`;
+
+export const Field = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 7fr 1fr 1fr;
+  gap: 1rem;
+  height: 5.3rem;
+  padding: 1.5rem 0;
+  text-align: center;
+  border-bottom: 1px solid ${COLOR.black};
+`;
+
+export const List = styled.ul`
   margin-bottom: 4rem;
   li {
     display: grid;
@@ -14,7 +28,12 @@ export const List = styled.ul`
     padding: 1.5rem 0;
     text-align: center;
   }
-  li:first-child {
-    border-bottom: 1px solid ${COLOR.black};
-  }
+`;
+
+export const NoneYetWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 26.5rem;
+  border-bottom: 1px solid ${COLOR.greyC4};
 `;
