8f663f34df44659d7bb9d17a71bf36833cde04f0
Chun-gu <chungu.yi92@gmail.com>
2022-06-24 23:57:36 +0900

    ✨ ReviewListLoader export

--- src/components/Loader/index.ts
+++ src/components/Loader/index.ts
@@ -1,2 +1,3 @@
 export { default as ProductsListLoader } from "./ProductsListLoader";
 export { default as CommentListLoader } from "./CommentListLoader";
+export { default as ReviewListLoader } from "./ReviewListLoader";
