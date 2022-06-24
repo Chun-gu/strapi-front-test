526cddcc323515b168fc238ecb77508e08056016
Chun-gu <chungu.yi92@gmail.com>
2022-06-24 23:54:59 +0900

    👽️ getReviews에 limit, page 쿼리 추가

--- src/utils/api/review.ts
+++ src/utils/api/review.ts
@@ -1,9 +1,9 @@
 import { IAddReviewValues, IIdArg } from "src/types";
 import axios from "axios";
 
-export const getReviews = async (id: IIdArg = "") => {
+export const getReviews = async (id: IIdArg = "", limit = 5, page = 1) => {
   const { data } = await axios.get(
-    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/product-reviews/${id}`,
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/product-reviews/${id}?limit=${limit}&page=${page}`,
   );
   return data;
 };
