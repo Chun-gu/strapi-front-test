0e6cdcd65b5b6dd321db4167fcc7d438f6601f1a
Chun-gu <chungu.yi92@gmail.com>
2022-06-26 23:58:09 +0900

    💄 portal에 스크롤 방지 간소화

--- src/components/Portal/index.tsx
+++ src/components/Portal/index.tsx
@@ -12,14 +12,10 @@ export function Portal({ children, selector }: IPortalProps) {
 
   useEffect(() => {
     document.body.style.cssText = `
-        position: fixed; 
-        top: -${window.scrollY}px;
-        overflow-y: scroll;
-        width: 100%;`;
+        overflow: hidden;`;
+
     return () => {
-      const scrollY = document.body.style.top;
       document.body.style.cssText = "";
-      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
     };
   }, []);
 
