056650082f24350155849af346b70ae1427a836b
Chun-gu <chungu.yi92@gmail.com>
2022-06-27 23:51:22 +0900

    💄 ReviewList 스타일 변경

--- src/components/ReviewList/styled.ts
+++ src/components/ReviewList/styled.ts
@@ -4,11 +4,20 @@ import { COLOR } from "@styles/color";
 export const ReviewSection = styled.div`
   position: relative;
   min-height: 66.4rem;
+  padding-bottom: 5.8rem;
 `;
 
 export const ReviewList = styled.ul`
   min-height: 60.6rem;
-  margin-bottom: 4rem;
+  border-top: 1px solid ${COLOR.greyC4};
+  border-bottom: 1px solid ${COLOR.greyC4};
+`;
+
+export const PaginationWrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 50%;
+  transform: translateX(50%);
 `;
 
 export const NoneYetWrapper = styled.div`
