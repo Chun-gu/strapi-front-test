3c366d2afba954636bd8833cc3a3d41ac9edae86
Chun-gu <chungu.yi92@gmail.com>
2022-07-02 23:57:23 +0900

    🩹 Alert 모달 컴포넌트 사용 방식 변경에 따른 변경

--- src/components/Modals/AddReviewModal/index.tsx
+++ src/components/Modals/AddReviewModal/index.tsx
@@ -6,7 +6,6 @@ import { addReview } from "@api";
 import { Buttons, Spinner, StarRating } from "@components";
 import { IAddReviewValues } from "@types";
 import { useModal } from "@hooks";
-import AlertModal from "../AlertModal";
 import ModalContainer from "../ModalContainer";
 import CloseIcon from "public/assets/icons/icon-delete.svg";
 import { Title, Wrapper, CloseButton, Input as input } from "../styled";
@@ -20,10 +19,12 @@ type AddReviewModalProps = { productId: string | string[] | undefined };
 
 const AddReviewModal = ({ productId }: AddReviewModalProps) => {
   const { data: session } = useSession();
-  const addReviewModal = useModal("addReviewModal");
-  const alertModal = useModal("alertModal");
 
   const [rating, setRating] = useState(1);
+
+  const addReviewModal = useModal("addReviewModal");
+  const postDoneModal = useModal("postDone");
+  const errorModal = useModal("error");
 
   const {
     reset,
@@ -41,14 +42,10 @@ const AddReviewModal = ({ productId }: AddReviewModalProps) => {
     onSuccess: () => {
       reset();
       queryClient.invalidateQueries(["getReviews"]);
-      alertModal.addText("작성을 완료했습니다.");
-      alertModal.open();
+      postDoneModal.open();
     },
     onError: () => {
-      alertModal.addText(
-        "작성에 실패했습니다. 잠시 후 다시 시도해주시기 바랍니다.",
-      );
-      alertModal.open();
+      errorModal.open();
     },
   });
 
@@ -70,55 +67,51 @@ const AddReviewModal = ({ productId }: AddReviewModalProps) => {
       console.log("AddReviewModal 언마운트");
       reset();
     };
-  }, [reset]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <>
-      <ModalContainer closeModal={handleClose}>
-        {isLoading && <Spinner />}
-        <Wrapper>
-          <Title>리뷰 작성</Title>
-          <StarRating rating={rating} readOnly={false} setRating={setRating} />
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-              <label htmlFor="image">이미지 첨부</label>
-              <input type="file" id="image" {...register("image")} />
-            </div>
-            <div>
-              <label htmlFor="content" className="sr-only">
-                리뷰 내용
-              </label>
-              <Input
-                id="content"
-                placeholder="리뷰를 작성하세요. (10자 ~ 200자)"
-                {...register("content", {
-                  required: true,
-                  minLength: 10,
-                  maxLength: 200,
-                })}
-              />
-            </div>
-            <Buttons.Custom
-              type="submit"
-              width={10}
-              height={3}
-              fontSize={1.6}
-              color="green"
-              disabled={!isValid || isLoading}
-            >
-              작성
-            </Buttons.Custom>
-          </form>
-          <CloseButton onClick={handleClose}>
-            <CloseIcon width={18} height={18} />
-            <span className="sr-only">리뷰 작성창 닫기</span>
-          </CloseButton>
-        </Wrapper>
-      </ModalContainer>
-      {alertModal.modal.isOpen && (
-        <AlertModal modalId="alertModal" onClose={handleClose} />
-      )}
-    </>
+    <ModalContainer closeModal={handleClose}>
+      {isLoading && <Spinner />}
+      <Wrapper>
+        <Title>리뷰 작성</Title>
+        <StarRating rating={rating} readOnly={false} setRating={setRating} />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label htmlFor="image">이미지 첨부</label>
+            <input type="file" id="image" {...register("image")} />
+          </div>
+          <div>
+            <label htmlFor="content" className="sr-only">
+              리뷰 내용
+            </label>
+            <Input
+              id="content"
+              placeholder="리뷰를 작성하세요. (10자 ~ 200자)"
+              {...register("content", {
+                required: true,
+                minLength: 10,
+                maxLength: 200,
+              })}
+            />
+          </div>
+          <Buttons.Custom
+            type="submit"
+            width={10}
+            height={3}
+            fontSize={1.6}
+            color="green"
+            disabled={!isValid || isLoading}
+          >
+            작성
+          </Buttons.Custom>
+        </form>
+        <CloseButton onClick={handleClose}>
+          <CloseIcon width={18} height={18} />
+          <span className="sr-only">리뷰 작성창 닫기</span>
+        </CloseButton>
+      </Wrapper>
+    </ModalContainer>
   );
 };
 
