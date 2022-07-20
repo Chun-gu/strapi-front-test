51002b43bc690f88c0d1f8e1b589187667f8d89d
Chun-gu <chungu.yi92@gmail.com>
2022-07-20 23:39:07 +0900

    🔥

--- src/components/InquiryList/index.tsx
+++ src/components/InquiryList/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
